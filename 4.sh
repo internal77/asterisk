54542d25386f45ccf8fbf03f4de39ff5ef37885c
internal77 <73139884+internal77@users.noreply.github.com>
2021-02-14 10:48:12 +0200

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,2 +1,4 @@
 sudo wget https://downloads.mysql.com/archives/get/p/10/file/mysql-connector-odbc-5.3.13-linux-ubuntu18.04-x86-64bit.tar.gz
 sudo tar -xzvf mysql-connector-odbc-5.3.13-linux-ubuntu18.04-x86-64bit.tar.gz
+sudo mkdir /usr/lib/odbc
+sudo cp mysql-connector-odbc-5.3.13-linux-ubuntu18.04-x86-64bit/lib/* /usr/lib/odbc/
